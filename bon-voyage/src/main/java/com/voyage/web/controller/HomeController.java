26d2aea517516a10182c971dac089b27fb1b2c01
Rajesh KV <rajesh.k.v@orbitz.com>
2013-10-25 04:23:42 -0500

    Spring MVC

---
+++ bon-voyage/src/main/java/com/voyage/web/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.voyage.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+ 
+        model.addAttribute("message", "Spring 3 MVC Hello World");
+        System.out.println("CONTROLLER CALLED");
+        return "hello";
+        
+    }
+}
